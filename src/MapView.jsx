d9698f7e5774dd661f753269df3505844c132588
snails <tim0988045205@gmail.com>
2026-01-16 00:36:07 +0800

    update resolve api to echo url and host

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
 
 function JumpToUser({ userLocation, jumpToUserSignal }) {
   const map = useMap();
@@ -24,6 +29,13 @@ function JumpToTarget({ flyToTarget, flyToSignal }) {
   return null;
 }
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function MapView({ places, userLocation, jumpToUserSignal, userMappingFilter,flyToTarget, flyToSignal }) {
 
